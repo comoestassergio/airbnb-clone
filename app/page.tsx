35ffbed2797fc1a1a06795e1f8635bed2554a29d
Serge <93369654+comoestassergio@users.noreply.github.com>
2023-04-21 12:22:30 +0300

    Initial setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Nunito } from 'next/font/google'
+
+const nunito = Nunito({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>Hello</div>
+  )
+}
