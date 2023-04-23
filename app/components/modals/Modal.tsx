da108626c149f99ef7fe6be17ac5ed73d4f698d2
Serge <93369654+comoestassergio@users.noreply.github.com>
2023-04-23 12:28:35 +0300

    Fix footer

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -155,6 +155,7 @@ const Modal: React.FC<ModalProps> = ({
                                             onClick={handleSubmit}
                                         />
                                     </div>
+                                    {footer}
                                 </div>
                             </div>
                         </div>
